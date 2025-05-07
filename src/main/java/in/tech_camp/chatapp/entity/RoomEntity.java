759cc401ccac553066af6ec7b2e5deef1bbdaaeb
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-05-07 16:17:30 +0900

    :sparkels: チャットルームのテーブルとエンティティ、リポジトリ作成

---
+++ src/main/java/in/tech_camp/chatapp/entity/RoomEntity.java
@@ -0,0 +1,9 @@
+package in.tech_camp.chatapp.entity;
+
+import lombok.Data;
+
+@Data
+public class RoomEntity {
+  private Integer id;
+  private String name;
+}
---
+++ src/main/resources/db/migration/V3__create_rooms_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE rooms (
+  id SERIAL NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  PRIMARY KEY(id)
+)
