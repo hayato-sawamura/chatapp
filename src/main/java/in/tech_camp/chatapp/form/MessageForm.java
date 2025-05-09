12fe88a93b3aa1268990e81e808158f5ba33d7f0
hayato.sawamura <hayato.sawamura@smart-gruop.co.jp>
2025-05-09 11:55:29 +0900

    :zap: アップロードした画像ファイルを保存する機能作成

--- src/main/java/in/tech_camp/chatapp/form/MessageForm.java
+++ src/main/java/in/tech_camp/chatapp/form/MessageForm.java
@@ -1,5 +1,7 @@
 package in.tech_camp.chatapp.form;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import in.tech_camp.chatapp.validation.ValidationPriority1;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -8,5 +10,5 @@ import lombok.Data;
 public class MessageForm {
   @NotBlank(message="This content is empty", groups=ValidationPriority1.class)
   private String content;
-  // private String image; 
+  private MultipartFile image; 
 }
